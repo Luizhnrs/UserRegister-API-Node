ddeb361f176833239ab676f2ba87d0a3b903ddef
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-10 20:17:58 -0300

    Feat: Server Full Config

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,21 @@ import fastifyCors from "@fastify/cors";
 import cors from "@fastify/cors"
 
 const app = fastify ({ logger: true });
+
+app.setErrorHandler((error, request, reply) => {
+    reply.code(400).send({message: error.message})
+})
+
+const start = async () => {
+
+    await app.register(cors);
+    await app.register(routes);
+
+    try{
+        await app.listen({port: 8080})
+    }catch(err){
+        process.exit(1)
+    }
+}
+
+start();
