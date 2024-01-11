db56dafdf4a281fd5284376febfc31cd2fc48305
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-11 15:42:50 -0300

    TODO: Tailwind Config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
-export function App(){
+export default function App(){
   return (
-    <div>
-      <h1>Users React APP</h1>
+    <div className="">
+      <main>
+        
+      </main>
     </div>
   )
 }
