b4ca8f5222b8981c04fcb3bc40d097becab93654
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-10 21:39:23 -0300

    Feat: User Post Route in route

--- src/route.ts
+++ src/route.ts
@@ -1,8 +1,14 @@
 import { ok } from "assert";
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { CreateUserController } from "./prisma/controller/CreateUserController";
 
 export async function routes(fastify: FastifyInstance, option: FastifyPluginOptions) {
+    
     fastify.get("/home", async (request: FastifyRequest, reply: FastifyReply) =>{
         return {ok: true}
+    }) 
+
+    fastify.post("/person",async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateUserController().handle(request, reply);	
     })
 }
